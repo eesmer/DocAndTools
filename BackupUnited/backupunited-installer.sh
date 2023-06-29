5a7f26d0255b0d4a9271970f8cfe12ace97af955
erkan.esmer <erkanesmer@gmail.com>
2023-06-29 10:55:30 +0300

    installer updated

--- BackupUnited/backupunited-installer.sh
+++ BackupUnited/backupunited-installer.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-apt-get -y install rsync ssmtp mutt
-apt-get -y install cifst-utils smbclient
-apt-get -y install tree
+if [ -x "$(command -v rsync)" ] || [ -x "$(command -v ssmtp)" ] || [ -x "$(command -v mutt)" ] || \
+    [ -x "$(command -v cifs-utils)" ] || [ -x "$(command -v smbclient)" ] || [ -x "$(command -v tree)" ]; then
 
-mkdir -p /usr/local/backup-united
-mkdir /usr/local/backup-united/backup-scripts
-mkdir /usr/local/backup-united/backups
+    apt-get -y install rsync ssmtp mutt
+    apt-get -y install cifst-utils smbclient
+    apt-get -y install tree
+
+    mkdir -p /usr/local/backup-united
+    mkdir /usr/local/backup-united/backup-scripts
+    mkdir /usr/local/backup-united/backups
+
 cat > /usr/local/backup-united/mail-message <<EOF
 -------------------------------------------
 | Backup-United Mail Notification         |
 -------------------------------------------
-
 EOF
+fi
