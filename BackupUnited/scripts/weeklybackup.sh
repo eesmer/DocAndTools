63f00868d47de61b6c4c376d4ce845496b15f505
erkan.esmer <erkanesmer@gmail.com>
2023-08-13 20:27:14 +0300

    add scripts dir

---
+++ BackupUnited/scripts/weeklybackup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#-----------------------------------------------------------
+# $1 BACKUPNAME
+#-----------------------------------------------------------
+
+WDIR="/usr/local/backupunited/backups"
+
+JOCKER=$
+#tar -czf /usr/local/backupunited/backups/$1-"$JOCKER(date +%Y%m%d-%H%M).tar.gz" /usr/local/backupunited/backups/weekly/$1
+mkdir -p /usr/local/backupunited/backups/weekly/
+tar -czf /usr/local/backupunited/backups/weekly/$1-"$(date +%Y%m%d-%H%M).tar.gz" /usr/local/backupunited/backups/$1
