d6b22c9869f691b208fdb8079522eb590e69b43f
erkan.esmer <erkanesmer@gmail.com>
2023-09-25 20:33:36 +0300

    isocreator-installer added

---
+++ iso-creator/isocreator-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WDIR=/usr/local/isogen
+
+apt-get -y install wget
+
+mkdir -p $WDIR
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/create-iso.sh -O $WDIR/create-iso.sh -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/preseed.cfg -O $WDIR/preseed.cfg -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/menu.cfg -O $WDIR/menu.cfg -q
+
+echo "iso-creator app installation completed.."
+echo -e
+echo "[ for usage ]"
+echo "cd $WDIR"
+echo "bash create-iso.sh ISOFILEPATH/ISOFILENAME.iso preseed.cfg menu.cfg NEW_NAME-Custom.iso"
