5c60b3d8af5c64d7d87edbb446cc62ca0b4e99e5
erkan.esmer <erkanesmer@gmail.com>
2023-09-26 20:13:01 +0300

    dir renamed

--- Debian-iso_Creator/isocreator-installer.sh
+++ Debian-iso_Creator/isocreator-installer.sh
@@ -5,10 +5,10 @@ WDIR=/usr/local/isogen
 apt-get -y install wget
 
 mkdir -p $WDIR
-wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/create-iso.sh -O $WDIR/create-iso.sh -q
-wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/preseed.cfg -O $WDIR/preseed.cfg -q
-wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/menu.cfg -O $WDIR/menu.cfg -q
-wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/splash.png -O $WDIR/splash.png -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/Debian-iso_Creator/create-iso.sh -O $WDIR/create-iso.sh -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/Debian-iso_Creator/preseed.cfg -O $WDIR/preseed.cfg -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/Debian-iso_Creator/menu.cfg -O $WDIR/menu.cfg -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/Debian-iso_Creator/splash.png -O $WDIR/splash.png -q
 
 echo "iso-creator app installation completed.."
 echo -e
