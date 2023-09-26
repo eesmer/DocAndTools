1b7c24ee6b11106b879a97d9de79b41204de5de0
erkan.esmer <erkanesmer@gmail.com>
2023-09-26 16:04:57 +0300

    splash.png download added

--- iso-creator/isocreator-installer.sh
+++ iso-creator/isocreator-installer.sh
@@ -8,6 +8,7 @@ mkdir -p $WDIR
 wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/create-iso.sh -O $WDIR/create-iso.sh -q
 wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/preseed.cfg -O $WDIR/preseed.cfg -q
 wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/menu.cfg -O $WDIR/menu.cfg -q
+wget https://raw.githubusercontent.com/eesmer/DocAndTools/master/iso-creator/splash.png -O $WDIR/splash.png -q
 
 echo "iso-creator app installation completed.."
 echo -e
