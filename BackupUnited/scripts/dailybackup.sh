60b987c562cc5f2570587d2e54b9a42763ba9b68
erkan.esmer <erkanesmer@gmail.com>
2023-11-12 07:56:53 +0300

    dailybackup.sh edited

--- BackupUnited/scripts/dailybackup.sh
+++ BackupUnited/scripts/dailybackup.sh
@@ -7,5 +7,5 @@
 WDIR="/usr/local/backupunited/backups"
 
 JOCKER=$
-mkdir -p /usr/local/backupunited/backups/daily/
-tar -czf /usr/local/backupunited/backups/daily/$1-"$(date +%Y%m%d-%H%M).tar.gz" /usr/local/backupunited/backups/sync/$1
+mkdir -p $WDIR/daily
+tar -czf $WDIR/daily/$1-"$(date +%Y%m%d-%H%M).tar.gz" $WDIR/sync/$1
