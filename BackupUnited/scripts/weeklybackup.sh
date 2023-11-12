90daa83aff507e7314076ec76c547f1131a569ae
erkan.esmer <erkanesmer@gmail.com>
2023-11-12 07:57:58 +0300

    weeklybackup.sh edited

--- BackupUnited/scripts/weeklybackup.sh
+++ BackupUnited/scripts/weeklybackup.sh
@@ -7,5 +7,5 @@
 WDIR="/usr/local/backupunited/backups"
 
 JOCKER=$
-mkdir -p /usr/local/backupunited/backups/weekly/
-tar -czf /usr/local/backupunited/backups/weekly/$1-"$(date +%Y%m%d-%H%M).tar.gz" /usr/local/backupunited/backups/sync/$1
+mkdir -p $WDIR/weekly
+tar -czf $WDIR/weekly/$1-"$(date +%Y%m%d-%H%M).tar.gz" $WDIR/sync/$1
