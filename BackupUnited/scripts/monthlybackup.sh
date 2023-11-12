af0e41e7d181612f980a369b18ee313d1b4a33c6
erkan.esmer <erkanesmer@gmail.com>
2023-11-12 07:59:34 +0300

    monthlybackup.sh edited

--- BackupUnited/scripts/monthlybackup.sh
+++ BackupUnited/scripts/monthlybackup.sh
@@ -7,5 +7,5 @@
 WDIR="/usr/local/backupunited/backups"
 
 JOCKER=$
-mkdir -p /usr/local/backupunited/backups/monthly/
-tar -czf /usr/local/backupunited/backups/monthly/$1-"$(date +%Y%m%d-%H%M).tar.gz" /usr/local/backupunited/backups/sync/$1
+mkdir -p $WDIR/monthly
+tar -czf $WDIR/monthly/$1-"$(date +%Y%m%d-%H%M).tar.gz" $WDIR/sync/$1
