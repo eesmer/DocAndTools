24cb0263272c79ce826d67f4bc06406dfa6b8468
erkan.esmer <erkanesmer@gmail.com>
2024-07-05 21:56:26 +0300

    SambaAD installer script edited

--- SambaAD-HelperScripts/samba-activedirectory-installer.sh
+++ SambaAD-HelperScripts/samba-activedirectory-installer.sh
@@ -4,3 +4,16 @@
 # This script has been tested in Debian environment.
 # It is compatible with Debian.
 #-------------------------------------------------------------------
+
+# Color Codes
+MAGENTA="tput setaf 1"
+GREEN="tput setaf 2"
+YELLOW="tput setaf 3"
+DGREEN="tput setaf 4"
+CYAN="tput setaf 6"
+WHITE="tput setaf 7"
+GRAY="tput setaf 8"
+RED="tput setaf 9"
+BLUE="tput setaf 12"
+NOCOL="tput sgr0"
+BOLD="tput bold"
