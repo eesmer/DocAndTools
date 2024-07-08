67c97e8d24818117c4f709466cc9efae5199158f
erkan.esmer <erkanesmer@gmail.com>
2024-07-08 21:38:56 +0300

    Active Directory installer script edited

--- SambaAD-HelperScripts/samba-activedirectory-installer.sh
+++ SambaAD-HelperScripts/samba-activedirectory-installer.sh
@@ -102,6 +102,12 @@ SAMBAAD_INSTALL() {
 	apt-get -y install dnsutils net-tools
 	#apt-get -y install ack expect krb5-user krb5-config
 	#apt-get -y install curl wget
+	
+	rm /etc/samba/smb.conf
+	samba-tool domain provision --server-role=dc --use-rfc2307 --realm="$REALM" --domain="$DOMAIN" --adminpass="$PASSWORD"
+	sed -i '/server services =/a log level = 4' /etc/samba/smb.conf
+	sed -i '/log level =/a log file = /var/log/samba/$REALM.log' /etc/samba/smb.conf
+	sed -i '/log file =/a debug timestamp = yes' /etc/samba/smb.conf
 }
 
 CHECK_DISTRO
