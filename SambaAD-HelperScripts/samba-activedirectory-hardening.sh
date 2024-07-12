ae59ce6baef6e695d2a992e9486f12c1b532444e
erkan.esmer <erkanesmer@gmail.com>
2024-07-12 21:08:35 +0300

    hardening script added

---
+++ SambaAD-HelperScripts/samba-activedirectory-hardening.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------------
+# This script makes security changes to the Samba Active Directory configuration.
+# Please backup, test and apply.
+#
+# The script must run on the Samba Active Directory Domain Controller.
+#------------------------------------------------------------------------------------
