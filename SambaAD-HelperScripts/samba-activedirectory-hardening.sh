bd15118248f537d816b9f0b479ca09eb636c27e4
erkan.esmer <erkanesmer@gmail.com>
2024-07-12 21:09:05 +0300

    hardening script added

--- SambaAD-HelperScripts/samba-activedirectory-hardening.sh
+++ SambaAD-HelperScripts/samba-activedirectory-hardening.sh
@@ -6,3 +6,8 @@
 #
 # The script must run on the Samba Active Directory Domain Controller.
 #------------------------------------------------------------------------------------
+# This script has been tested only on DebianDC installations and is tailored to DebianDC configurations.
+# DebianDC makes these settings by default.
+# https://github.com/eesmer/DebianDC
+#------------------------------------------------------------------------------------
+
