7ba0695f73f80d840222ec37a274b16fd9489b22
erkan.esmer <erkanesmer@gmail.com>
2024-10-28 12:04:20 +0300

    MyPCTools dir created

---
+++ MyPCTools/my_debian_pc_after-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cat > /etc/apt/sources.list << EOF
+deb https://deb.debian.org/debian/ bookworm contrib main non-free non-free-firmware
+deb-src https://deb.debian.org/debian/ bookworm contrib main non-free non-free-firmware
+deb https://deb.debian.org/debian/ bookworm-updates contrib main non-free non-free-firmware
+deb-src https://deb.debian.org/debian/ bookworm-updates contrib main non-free non-free-firmware
+deb https://security.debian.org/debian-security/ bookworm-security contrib main non-free non-free-firmware
+deb-src https://security.debian.org/debian-security/ bookworm-security contrib main non-free non-free-firmware
+deb http://deb.debian.org/debian/ bookworm-backports main non-free contrib
+deb-src http://deb.debian.org/debian/ bookworm-backports main non-free contrib
+EOF
+
+apt update
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | gpg --dearmor -o /usr/share/keyrings/virtualbox.gpg
+echo "deb [signed-by=/usr/share/keyrings/virtualbox.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" >> /etc/apt/sources.list
+
+apt-get -y install $(apt search ^firmware- 2> /dev/null | grep ^firmware | grep -v micropython-dl | cut -d "/" -f 1)
+apt-get -y install i3 xtrlock thunar zsh
+apt-get -y install vim tmux openssh-server htop
+apt-get -y install feathernotes atril pavucontrol unzip xfce4-terminal freerdp2-x11 vlc
+apt-get -y install firefox-esr chromium
+apt-get -y install libreoffice-writer libreoffice-calc
+apt-get -y install git
+apt-get -y install ack ack-grep wget curl rsync dnsutils whois
+apt-get -y install gnupg2
+apt-get -y install openvpn
+apt-get -y install encfs ntfs-3g
+apt-get -y install python-pip
+apt-get -y install python3 bpython3
+apt-get -y install python3-pip --install-recommends
+apt-get -y install software-properties-common
+apt-get -y install lsb-release wget apt-transport-https
