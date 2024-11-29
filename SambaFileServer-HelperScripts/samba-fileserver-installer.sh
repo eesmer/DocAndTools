aa9712818ba53107e8fb72a738b7e1db58f07501
erkan.esmer <erkanesmer@gmail.com>
2024-11-29 21:22:46 +0300

    samba-fileserver-installer.sh edited

--- SambaFileServer-HelperScripts/samba-fileserver-installer.sh
+++ SambaFileServer-HelperScripts/samba-fileserver-installer.sh
@@ -17,22 +17,24 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Install Samba Package
+apt install -y whiptail
 apt install -y samba
+apt install vim tmux htop
 
 SHARE_DIR=$(whiptail --inputbox "Please specify the Sharing Directory Path" 10 50 --title "Share Directory Path" --backtitle "Share Directory Path" 3>&1 1>&2 2>&3)
 SHARE_NAME=$(whiptail --inputbox "Please specify the Sharing Directory Name" 10 50 --title "Share Directory Name" --backtitle "Share Directory Name" 3>&1 1>&2 2>&3)
 SMB_USER=$(whiptail --inputbox "Please Enter Account Name for Samba User Account" 10 50 --title "Samba User Account Name" --backtitle "Samba User Account Name" 3>&1 1>&2 2>&3)
-SMB_PASS=$(whiptail --inputbox "Please Enter Password to be Defined to Samba User Account" 10 50 --title "Samba User Account Password" --backtitle "Samba User Account Password" 3>&1 1>&2 2>&3)
-
-# Create Sharing Directory
-mkdir -p "$SHARE_DIR"
-chmod -R 0775 "$SHARE_DIR"
-chown -R nobody:nogroup "$SHARE_DIR"
+SMB_PASS=$(whiptail --passwordbox "Please Enter Password to be Defined to Samba User Account" 10 50 --title "Samba User Account Password" --backtitle "Samba User Account Password" 3>&1 1>&2 2>&3)
 
 # Create Samba User Account
 useradd -M -s /sbin/nologin "$SMB_USER"
 (echo "$SMB_PASS"; echo "$SMB_PASS") | smbpasswd -a "$SMB_USER"
 smbpasswd -e "$SMB_USER"
+
+# Create Sharing Directory
+mkdir -p "$SHARE_DIR"
+chmod -R 0775 "$SHARE_DIR"
+chown -R $SMB_USER:$SMB_USER "$SHARE_DIR"
 
 # Generate Samba Configuration
 cat > "$SMB_CONFIG" <<EOF
