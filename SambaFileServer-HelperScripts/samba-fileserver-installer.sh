7d71ebdcd7c2ca1aee24e1fd8ebcdc4f0bf6b820
erkan.esmer <erkanesmer@gmail.com>
2024-11-29 19:57:57 +0300

    SambaFileServer installer and create share script added

--- SambaFileServer-HelperScripts/samba-fileserver-installer.sh
+++ SambaFileServer-HelperScripts/samba-fileserver-installer.sh
@@ -11,4 +11,6 @@ apt install -y samba
 
 SHARE_DIR=$(whiptail --inputbox "Please specify the Sharing Directory Path" 10 50 --title "Share Directory Path" --backtitle "Share Directory Path" 3>&1 1>&2 2>&3)
 SHARE_NAME=$(whiptail --inputbox "Please specify the Sharing Directory Name" 10 50 --title "Share Directory Name" --backtitle "Share Directory Name" 3>&1 1>&2 2>&3)
+SMB_USER=$(whiptail --inputbox "Please Enter Account Name for Samba User Account" 10 50 --title "Samba User Account Name" --backtitle "Samba User Account Name" 3>&1 1>&2 2>&3)
+SMB_PASS=$(whiptail --inputbox "Please Enter Password to be Defined to Samba User Account" 10 50 --title "Samba User Account Password" --backtitle "Samba User Account Password" 3>&1 1>&2 2>&3)
 
